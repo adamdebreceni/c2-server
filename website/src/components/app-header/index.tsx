f1284a67773c3e0604e1c862ecd94fa9ed076772
Adam Debreceni <adebreceni@apache.org>
2025-01-09 11:00:44 +0100

    Make /agents the default page, hightlight location

--- website/src/components/app-header/index.tsx
+++ website/src/components/app-header/index.tsx
@@ -1,13 +1,14 @@
 import * as React from "react";
-import { useNavigate } from "react-router";
+import { useLocation, useNavigate } from "react-router";
 
 import "./index.scss";
 
 export function AppHeader() {
+  const path = useLocation().pathname;
   const navigate = useNavigate();
   return <div className="app-header">
-    <div className="item" onClick={()=>navigate("/agents")}>Agents</div>
-    <div className="item" onClick={()=>navigate("/agent-classes")}>Classes</div>
-    <div className="item" onClick={()=>navigate("/flows")}>Flows</div>
+    <div className={`item ${path === '/agents' ? 'active' : ''}`} onClick={()=>navigate("/agents")}>Agents</div>
+    <div className={`item ${path === '/agent-classes' ? 'active' : ''}`} onClick={()=>navigate("/agent-classes")}>Classes</div>
+    <div className={`item ${path === '/flows' ? 'active' : ''}`} onClick={()=>navigate("/flows")}>Flows</div>
   </div>;
 }
